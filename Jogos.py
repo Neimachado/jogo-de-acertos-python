08f6449d58652493a75d4746d31e51bee8dd7d6a
Neimachado <neiltonmerc@gmail.com>
2022-08-20 00:13:42 -0300

    Implementação do sistema de pontuação

--- Jogos.py
+++ Jogos.py
@@ -5,6 +5,7 @@ print("Bem vindo ao jogo de adivinhação")
 print("***********************************")
 numero_secreto = randrange(1, 101)
 total_de_tentativas = 3
+pontos = 1000
 
 print("Qual nível gostaria de jogar?")
 print("(1) Fácil (2) Médio (3) Difícil")
@@ -18,7 +19,7 @@ else:
     total_de_tentativas = 3
 for rodada in range (1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute = input("Digite um número entre 1 e 100")
+    chute = input("Digite um número entre 1 e 100 \n")
     print("Você digitou", chute)
 
     if(int(chute) < 1 or int(chute) > 100):
@@ -31,12 +32,14 @@ for rodada in range (1, total_de_tentativas + 1):
 
     if (acertou):
         print("Você acertou!!")
+        print(f"Você fez {pontos} pontos!")
         break
     else:
         if(chute_foi_maior):
             print("Seu chute foi maior que o número secreto!")
         elif(chute_foi_menor):
            print("Seu chute foi menor que o número secreto!")
- 
+        pontos_perdidos = abs(numero_secreto - int(chute))
+        pontos = pontos - pontos_perdidos
 print("Fim de Jogo!!!")
 
