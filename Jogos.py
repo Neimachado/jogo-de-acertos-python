aa71a585e3e9a85514674d7d4e5e8a95dc134cc4
Neimachado <neiltonmerc@gmail.com>
2022-08-19 23:24:19 -0300

    Alteração do laço de While para For e incremento do break

--- Jogos.py
+++ Jogos.py
@@ -1,16 +1,21 @@
+from turtle import clear
+
+
 print("***********************************")
 print("Bem vindo ao jogo de adivinhação")
 print("***********************************")
 
 numero_secreto = 33
 total_de_tentativas = 3
-rodada = 1
 
-
-while(rodada <= total_de_tentativas):
+for rodada in range (1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute = input("Digite um valor")
+    chute = input("Digite um número entre 1 e 100")
     print("Você digitou", chute)
+
+    if(int(chute) < 1 or int(chute) > 100):
+        print("Você deve digitar um número entre 1 e 100")
+        continue
 
     acertou = numero_secreto == int(chute)
     chute_foi_maior = int(chute) > numero_secreto
@@ -18,12 +23,12 @@ while(rodada <= total_de_tentativas):
 
     if (acertou):
         print("Você acertou!!")
+        break
     else:
         if(chute_foi_maior):
             print("Seu chute foi maior que o número secreto!")
         elif(chute_foi_menor):
            print("Seu chute foi menor que o número secreto!")
-    rodada = rodada + 1
-
+ 
 print("Fim de Jogo!!!")
 
